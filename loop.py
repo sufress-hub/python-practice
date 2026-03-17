b2e6d1512e26e06394ba743923a00a45c913b20e
sufress-hub <sufress60785@gmail.com>
2026-03-17 20:26:17 +0530

    Update loop.py

--- loop.py
+++ loop.py
@@ -2,3 +2,11 @@ for a in range(1,6):
         for b in range(1,a+1):
                 print(a,end='')
         print()
+
+#The output of this program is something like this I am currently learning loops
+#
+1
+12
+123
+1234
+12345
