0deb5f5a28b299ba7d6c4dda323af6aec62f8121
sufress-hub <sufress60785@gmail.com>
2026-04-19 10:22:33 +0530

    Create listcomprehension.py

---
+++ listcomprehension.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+squares = [num * num for num in numbers]
+
+print(squares)  # [1, 4, 9, 16, 25]
+
+
+numbes = [1, 2, 3, 4, 5]
+multyply = [num * 2 for num in numbes ]
+
+print(multyply)
+
+# Comprehension se naya list banao
+# jisme har number 2 se multiply ho
+# [2, 4, 6, 8, 10] aana chahiye
