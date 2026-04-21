11ffb15038b99474d13f6b539b3aef2bcc2d2d9a
sufress-hub <sufress60785@gmail.com>
2026-04-21 08:07:37 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,3 +1,9 @@
 with open("diary.txt", "r") as file:
     content = file.read()
     print(content)
+
+
+with open("diary.txt", "r") as file:
+#                       "r" = read mode
+    content = file.read()  # file ka saara content uthao
+    print(content)         # screen pe dikha do
